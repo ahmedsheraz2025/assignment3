f99e722be6512f03d0ef780e016a0dd984eda7a1
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-08-20 10:38:27 +0500

    assignment3

---
+++ assignment3.py
@@ -0,0 +1,42 @@
+myBrith = 2004
+currentYear = 2024
+myAge = currentYear - myBrith
+print("My age is", myAge)
+print("My brith year", myBrith)
+
+
+width = 4.0
+length = 3.0
+areaOfRectangle = width  * length
+print("Area of rectangle", areaOfRectangle)
+
+
+pi = 3.14159
+radius = 5
+area = pi * radius**2.
+print(area)
+
+
+l = 5 
+b = 5 
+h = 5
+a = l = b = h 
+surfacearea = 6*(a*a)
+print("Surface Area of Cube is:", surfacearea)
+
+
+Fahrenheit = 100
+Celsius = (Fahrenheit - 32) * 5/9
+print(Celsius,)
+
+
+givenNumber = int(input())
+intoMin = givenNumber // 60
+print(intoMin, givenNumber)
+
+
+marks = 60
+totalMarks = 100
+percentage = marks/totalMarks * 100
+print(percentage)
+
