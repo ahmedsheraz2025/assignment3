f58fb0dd21468c167029f46cd7e579740c008698
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-08-20 13:10:26 +0500

    update

--- assignment3.py
+++ assignment3.py
@@ -1,20 +1,20 @@
 myBrith = 2004
 currentYear = 2024
 myAge = currentYear - myBrith
-print("My age is", myAge)
-print("My brith year", myBrith)
+print("My age is:", myAge)
+print("My brith year:", myBrith)
 
 
 width = 4.0
 length = 3.0
 areaOfRectangle = width  * length
-print("Area of rectangle", areaOfRectangle)
+print("Area of rectangle:", areaOfRectangle)
 
 
 pi = 3.14159
 radius = 5
 area = pi * radius**2.
-print(area)
+print("Area of circle:", area)
 
 
 l = 5 
@@ -27,16 +27,31 @@ print("Surface Area of Cube is:", surfacearea)
 
 Fahrenheit = 100
 Celsius = (Fahrenheit - 32) * 5/9
-print(Celsius,)
+print("Celsius:", Celsius)
 
 
 givenNumber = int(input())
 intoMin = givenNumber // 60
-print(intoMin, givenNumber)
+print(intoMin, "min", givenNumber, "sec")
 
 
 marks = 60
 totalMarks = 100
 percentage = marks/totalMarks * 100
-print(percentage)
+print(percentage,"%")
+
+
+height = 25
+weight = 15
+BMI = weight / height**2
+print( height, "meter")
+print( weight,"kilograms")
+print("BMI:", BMI)
+
+
+height = 7
+radius = 1.5
+pi = 3.14159
+volume = pi * (radius ** 2) * height 
+print("Volume of cylinder:" ,volume)
 
